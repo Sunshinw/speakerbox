b9f1a0fb953ce86be0950753aa75833453cb25e2
Eakbodin Kanjanamuengtong <107537958+Sunshinw@users.noreply.github.com>
2026-03-07 14:40:37 +0700

    edit execute file for setting template for .sh

--- run_local.bat
+++ run_local.bat
@@ -1,3 +1,3 @@
 @echo off
-python RUN_THIS_FILE.py windows --dataset "C:\Users\sunshine\Desktop\DS_10283_3443\VCTK-Corpus-0.92\wav16_standardized" --output "exps/speakerbox_vctk"
+python RUN_THIS_FILE.py windows --dataset "C:\Users\sunshine\Desktop\DS_10283_3443\VCTK-Corpus-0.92\wav16_standardized" --output "exps/speakerbox_vctk" --train_mode "scratch"  
 pause
--- run_local.sh
+++ run_local.sh
@@ -2,5 +2,6 @@
 
 # Run the training script
 python RUN_THIS_FILE.py mac \
-  --dataset "/mnt/c/Users/sunshine/Desktop/DS_10283_3443/VCTK-Corpus-0.92/wav16_standardized" \
-  --output "exps/speakerbox_vctk"
+  --dataset "" \
+  --output "exps/" \
+  --train_mode "scratch" \
