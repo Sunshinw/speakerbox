6c53ea8319515fd0c9499e80e7d396f05db0e323
Eakbodin Kanjanamuengtong <107537958+Sunshinw@users.noreply.github.com>
2026-03-07 14:28:50 +0700

    delete unused files and add execute files

---
+++ run_local.bat
@@ -0,0 +1,3 @@
+@echo off
+python RUN_THIS_FILE.py windows --dataset "C:\Users\sunshine\Desktop\DS_10283_3443\VCTK-Corpus-0.92\wav16_standardized" --output "exps/speakerbox_vctk"
+pause
---
+++ run_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run the training script
+python RUN_THIS_FILE.py mac \
+  --dataset "/mnt/c/Users/sunshine/Desktop/DS_10283_3443/VCTK-Corpus-0.92/wav16_standardized" \
+  --output "exps/speakerbox_vctk"
